ee5275947cb71c01143ef7e4ff0a43345c0bf839
Glenly Pilapil <glenly.pilapil@gmail.com>
2019-03-28 20:11:10 +0800

    added users seed

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,18 +12,31 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-        	'name' => 'Glenly Pilapil',
-        	'email' => 'glenly.pilapil@gmail.com',
-        	'isAdmin' => 'true',
-        	'password' => 'glenly1234'
-        	]);
-
-        DB::table('users')->insert([
-        	'name' => 'Charles Ambridge',
-        	'email' => 'caambridge0328@yahoo.com',
-        	'isAdmin' => 'false',
-        	'password' => 'charles1234'
-        	]);
+        DB::table('users')->insert(array (
+            0 => 
+            array (
+                'id' => 1,
+                'name' => 'Administrator',
+                'email' => 'admin@company.com',
+                'email_verified_at' => NULL,
+                'password' => '$2y$10$1wTnC.R9W3DXcWAom.yQAu.f9UGTHAp5wcz9DmLE1yF6hKv8xgiq.',
+                'isAdmin' => 'true',
+                'remember_token' => NULL,
+                'created_at' => '2019-03-28 11:29:59',
+                'updated_at' => '2019-03-28 11:29:59',
+            ),
+            1 => 
+            array (
+                'id' => 2,
+                'name' => 'Employee',
+                'email' => 'employee@company.com',
+                'email_verified_at' => NULL,
+                'password' => '$2y$10$cT3.OZZo.rY7RrLZneZhlu6BQt3iBrABfu9.va6zk7ABNyMRNaaXi',
+                'isAdmin' => 'false',
+                'remember_token' => NULL,
+                'created_at' => '2019-03-28 11:30:55',
+                'updated_at' => '2019-03-28 11:30:55',
+            ),
+        ));
     }
 }
