af8cb8c77fdb1c1b899cd3b9914b840021abe451
Glenly Pilapil <glenly.pilapil@gmail.com>
2019-03-28 12:00:54 +0800

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Glenly
                 </div>
 
                 <div class="links">
