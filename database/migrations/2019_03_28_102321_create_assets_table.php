e22e39ed3da7dd4c13cfc6cd94d920068caa8db4
Glenly Pilapil <glenly.pilapil@gmail.com>
2019-03-28 18:43:43 +0800

    assets foreign key

--- database/migrations/2019_03_28_102321_create_assets_table.php
+++ database/migrations/2019_03_28_102321_create_assets_table.php
@@ -17,8 +17,16 @@ class CreateAssetsTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->unsignedBigInteger('category_id');
-            
+            $table->unsignedBigInteger('type_id');
             $table->timestamps();
+
+            $table->foreign('category_id')
+            ->references('id')->on('categories')
+            ->onDelete('restrict')->onUpdate('cascade');
+
+            $table->foreign('type_id')
+            ->references('id')->on('types')
+            ->onDelete('restrict')->onUpdate('cascade');
         });
     }
 
